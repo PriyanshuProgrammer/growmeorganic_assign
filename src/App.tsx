1333a70cae075491111358a73d87e9c25bde2dce
Priyanshu <priyanshu22600@gmail.com>
2024-12-16 20:58:45 +0530

    Error Solve

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { Tag } from 'primereact/tag';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
